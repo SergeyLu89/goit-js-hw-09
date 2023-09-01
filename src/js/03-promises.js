0c9ebb366614737551b99513873bd015463a33f4
SergeyLu89 <lytsenko.89@gmail.com>
2023-09-01 17:55:51 +0300

    task_3_done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+refs.form.addEventListener('submit', onSubmitButton);
+
+function onSubmitButton(event) {
+  event.preventDefault();
+  for (let i = 1; i <= refs.amount.value; i++) {
+    let delay = Number(refs.delay.value) + Number(refs.step.value) * (i - 1);
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position: position, delay: delay });
+      } else {
+        rej({ position: position, delay: delay });
+      }
+    }, delay);
+  });
 }
